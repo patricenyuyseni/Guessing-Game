a8efe6aa3f84aeff2908654c7ed1931b2395aff8
Patrice Nyuyseni <patriceflezy123@gmail.com>
2025-11-21 10:29:02 +0100

    code errors change

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 x=$(( 0 + RANDOM % (10 - 0 + 1 )))
  echo "random numbers between 0 to 10 : "
-read -p "enter the value you guest : " y
+read -r -p "enter the value you guest : " y
 
 if [[ $y -eq $x ]]; then 
  echo "congratulation you win"
